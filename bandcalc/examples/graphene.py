e85517cbff06705ba62e92e60520bd9f3d9fe88e
Niclas <niclas.goetting@t-online.de>
2020-05-29 00:41:16 +0200

    fix

--- bandcalc/examples/graphene.py
+++ bandcalc/examples/graphene.py
@@ -13,7 +13,7 @@ N = 1000
 b1 = np.array([2*np.pi/(np.sqrt(3)*a), 2*np.pi/a])
 b2 = np.array([2*np.pi/(np.sqrt(3)*a), -2*np.pi/a])
 b = np.vstack([b1, b2])
-lattice = bandcalc.generate_lattice_by_shell(b, 5)
+lattice = bandcalc.generate_lattice_by_shell(b, 2)
 
 # k path
 points = np.array([[0, 4*np.pi/(3*a)],         # K
@@ -34,7 +34,7 @@ fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(11,5))
 bandcalc.plot_lattice(ax[0], lattice, "ko")
 bandcalc.plot_k_path(ax[0], path, "r")
 voronoi_plot_2d(vor, ax[0], show_points=False, show_vertices=False)
-bandcalc.plot_bandstructure(ax[1], bandstructure, k_names, N, "k")
+bandcalc.plot_bandstructure(ax[1], bandstructure, k_names, "k")
 
 ax[1].set_ylim([-1, 30])
 
