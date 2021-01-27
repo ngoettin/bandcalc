1877175cadf36e715912a300d84ef6a99bb14da0
Niclas <niclas.goetting@t-online.de>
2021-01-27 17:26:43 +0100

    add 2d mode to wannier example

--- bandcalc/examples/wavefunction/wannier.py
+++ bandcalc/examples/wavefunction/wannier.py
@@ -1,6 +1,8 @@
 import argparse
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 from scipy.constants import physical_constants
 
 import bandcalc
@@ -15,11 +17,14 @@ parser.add_argument("-e", "--energy-level", type=int,
         default=0, help="energy level of the wave function")
 parser.add_argument("-s", "--shells", type=int,
         default=1, help="number of lattice shells for the calculation")
+parser.add_argument("--mode", type=str, choices=["2d", "3d"],
+    default="3d", help="draw wannier function in 2d or 3d")
 args = parser.parse_args()
 potential = args.potential
 angle = args.angle
 energy_level = args.energy_level
 shells = args.shells
+mode = args.mode
 
 a = lattice_constants["MoS2"]*1e9
 N = 1000
@@ -39,9 +44,6 @@ rec_moire_lattice = bandcalc.generate_lattice_by_shell(rec_m, shells)
 # Real space moire basis vectors
 m = bandcalc.generate_reciprocal_lattice_basis(rec_m)
 
-# Real space moire lattice vectors
-moire_lattice = bandcalc.generate_lattice_by_shell(m, shells)
-
 if potential == "MoS2":
     # Moire potential coefficients
     V = 6.6*1e-3*np.exp(-1j*94*np.pi/180) # in eV
@@ -51,15 +53,31 @@ elif potential == "off":
     potential_matrix = bandcalc.calc_potential_matrix(rec_moire_lattice)
 
 ## Calculate eigenstates for every k point in the MBZ
-k_points = bandcalc.generate_monkhorst_pack_set(m, 40).astype(np.float32)
+k_points = bandcalc.generate_monkhorst_pack_set(rec_m, 40).astype(np.float32)
 hamiltonian = bandcalc.calc_hamiltonian(rec_moire_lattice, potential_matrix, mass)
 
+# Real space moire lattice vectors
+moire_lattice = bandcalc.generate_lattice_by_shell(m, shells)
+
+# Wannier functions should be centered around potential minima
+moire_pot_lattice = moire_lattice + np.array([m[0, 0], m[0, 1]*1/3])
+
 ## Calculate the wannier function
-R = moire_lattice[3].astype(np.float32)
-r = bandcalc.generate_monkhorst_pack_set(m, 100)
+R = moire_pot_lattice[3].astype(np.float32)
+r = 3*bandcalc.generate_monkhorst_pack_set(m, 60)+R
 
 wannier_function = bandcalc.calc_wannier_function_gpu(
-        hamiltonian, k_points, rec_moire_lattice, r, R, band_index=0)
+        hamiltonian, k_points, rec_moire_lattice, r, R, band_index=energy_level)
 
 ## Plot the results
-bandcalc.plot_trisurface_3d(r[:,0], r[:,1], np.abs(wannier_function)/500)
+if mode == "3d":
+    bandcalc.plot_trisurface_3d(r[:,0], r[:,1], np.abs(wannier_function))
+elif mode == "2d":
+    fig, ax = plt.subplots()
+    contour = ax.tricontourf(r[:,0], r[:,1], np.abs(wannier_function), alpha=0.8)
+    bandcalc.plot_lattice(ax, bandcalc.generate_lattice_by_shell(m, 1), "ko")
+    ax.plot(R[0], R[1], "ro")
+    ax.set_xlabel("nm")
+    ax.set_ylabel("nm")
+    plt.colorbar(mappable=contour)
+    plt.show()
